def56a1c1dc98f00d225c12d347144b2e8e09b7d
SIONMATHEW <sayujion@gmail.com>
2026-03-13 14:59:50 +0800

    Logic: Add support for editing application status

--- src/main/java/seedu/interntrack/ApplicationList.java
+++ src/main/java/seedu/interntrack/ApplicationList.java
@@ -21,4 +21,23 @@ public class ApplicationList {
         userApplications.add(newApplication);
         return newApplication;
     }
+    /**
+     * Edits the status of an existing application.
+     *
+     * @param userApplications The list containing the application.
+     * @param index The 1-based index of the application to edit.
+     * @param status The new status value.
+     * @return The updated Application object.
+     * @throws InternTrackException If the index is invalid.
+     */
+    public static Application editApplicationStatus(ArrayList<Application> userApplications,
+                                                    int index, String status) throws InternTrackException {
+        if (index < 1 || index > userApplications.size()) {
+            throw new InternTrackException("Application index is out of range.");
+        }
+
+        Application application = userApplications.get(index - 1);
+        application.setStatus(status);
+        return application;
+    }
 }
